2eccc8317fd18591157903c205ed8231c4ec797c
oxumoron <a.suprun@i.ua>
2018-06-06 12:19:24 +0300

    carousel button

--- js/init.js
+++ js/init.js
@@ -9,7 +9,9 @@ $( function() {
     $('.reviews-carousel').slick({
         dots: true,
         autoplay: true,
-        autoplaySpeed: 5000
+        autoplaySpeed: 5000,
+        prevArrow: '<div class="slick-prev slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left"></i></div></div>',
+        nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-right"></i></div></div>'
     });
 
     // init slicknav
